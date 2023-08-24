625b941a22b43aca78a6452912f754cff0d900ea
Abhishek Thakur <85431526+iamabhishekt@users.noreply.github.com>
2023-08-23 22:28:31 -0400

    Update Heading.js

--- components/Heading.js
+++ components/Heading.js
@@ -1,6 +1,6 @@
 import Link from 'next/link'
 import { useRouter } from 'next/router'
-import { useSession } from 'next-auth/reac'
+import { useSession } from 'next-auth/react'
 
 export default function Heading() {
   const router = useRouter()
